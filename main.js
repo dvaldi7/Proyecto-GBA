5874131cde8a128fde9c1054017d217bd18ec7d2
Daniel Valdivieso-M <dvaldi7@gmail.com>
2024-08-23 01:10:20 +0200

    added clickable button to responsive

---
+++ main.js
@@ -0,0 +1,11 @@
+const myButton = document.querySelector(".button");
+const myNav = document.querySelector(".nav");
+
+myButton.addEventListener("click", () => {
+    myNav.classList.toggle("activo");
+    
+});
+
+
+
+
