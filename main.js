32384496ef8da1ddf8c0632c5da07674df99bb1d
Daniel Valdivieso-M <dvaldi7@gmail.com>
2024-09-14 17:55:09 +0200

    added slider

--- main.js
+++ main.js
@@ -8,6 +8,48 @@ myButton.addEventListener("click", () => {
     
 });
 
+/* SLIDER */ 
 
+let currentIndex = 0;
 
+document.querySelector('.prev-button').addEventListener('click', () => {
+    navigate(-1);
+});
+
+document.querySelector('.next-button').addEventListener('click', () => {
+    navigate(1);
+});
+
+function navigate(direction) {
+    const galleryContainer = document.querySelector('.slider-container');
+    const totalImages = document.querySelectorAll('.slider-item').length;
+    
+    currentIndex = (currentIndex + direction + totalImages) % totalImages;
+    const offset = -currentIndex * 100;
+    
+    galleryContainer.style.transform = `translateX(${offset}%)`;
+}
+
+//AUTOPLAY FOR SLIDER
+
+let autoplayInterval = null;
+
+function startAutoplay(interval) {
+    stopAutoplay();  // Detiene cualquier autoplay anterior para evitar múltiples intervalos.
+    autoplayInterval = setInterval(() => {
+        navigate(1);  // Navega a la siguiente imagen cada intervalo de tiempo.
+    }, interval);
+}
+
+function stopAutoplay() {
+    clearInterval(autoplayInterval);
+}
+
+// AUTOPLAY AFTER 3s
+startAutoplay(5000);
+
+// Opcional: Detener autoplay cuando el usuario interactúa con los botones de navegación.
+document.querySelectorAll('.nav-button').forEach(button => {
+    button.addEventListener('click', stopAutoplay);
+});
 
